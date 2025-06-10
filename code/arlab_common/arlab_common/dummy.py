7859c1d10bf6cf07c2cc011869b8fd0068dab7ee
Peter Viechter <peter.viechter@student.uni-augsburg.de>
2025-06-10 14:35:10 +0200

    Delete useless test + try linter

--- code/arlab_common/arlab_common/dummy.py
+++ code/arlab_common/arlab_common/dummy.py
@@ -1,5 +1,9 @@
+import os
+def banane(a, sdg, asdffffffffffffffffffffffffh, asfffffffffffffffffffffffffffffff, dsghdhsgsdg, asfgggggggggggggggggg):
+    pass
+
 def main():
-    print("Hi from arlab_common.")
+    print("Hi from arlab_common. as dasfas asf asf asf as fa fa fas fa fa fa fa fa fa fa fasf asf a fa ffa fa sfa f")
 
 
 if __name__ == "__main__":

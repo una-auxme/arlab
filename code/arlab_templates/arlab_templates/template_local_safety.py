7ec252912a5cff1c718b2a432a3f75ee38bbb7b0
Daniel Gabler <daniel.gabler@student.uni-augsburg.de>
2025-06-18 11:21:05 +0200

    consol scriped added, improved health_state publishing.  TODO: global_hearthbeat does not send node_id right now, this has to be adjusted later

--- code/arlab_templates/arlab_templates/template_local_safety.py
+++ code/arlab_templates/arlab_templates/template_local_safety.py
@@ -14,50 +14,58 @@ class LocalSafety(Node):
         # {int:nodeID: bool:is_alive}
         self.module_node_table = {}
         self.timer = self.create_timer(1.0, self.reset_module_node_table)
+        self.health_state = -1
+        self.pub_global_heartbeat = self.create_publisher(Int32, "/global_heartbeat", 10)
+        self.create_subscription(Int32, "/local_module_heartbeat", self.callback, 10)
 
-        self.pub_global_heartbeat = self.create_publisher(String, "/global_heartbeat", 10)
-        self.create_subscription(Int32, "/<local_module>_heartbeat", self.callback, 10)
-
-    def pub_module_heartbeat(self, msg: str):
+    def pub_module_heartbeat(self):
         """Publishes a global heartbeat to the Central Safety node
-
+            If heartbeat is not -1 or 0 the Central Safety Node will receive an error code
         Args:
-            msg (str): The message to publish
+            msg(Int32) = Encoded module health_state_error
         """
-        ros_msg = String(data=msg)
-        self.pub_global_heartbeat.publish(ros_msg)
+        msg = Int32()
+        msg.data = self.health_state
+        # print(f"Received message: debugHelper")
+        self.pub_global_heartbeat.publish(msg)
+        #reset node health_state
+        self.health_state = 0
 
     def callback(self, msg: Int32):
-        """Receives messages from the /<local_module>_heartbeat
+        """Receives messages from the /local_module_heartbeat
 
         Args:
             msg (Int32): Node ID of the publisher
         """
-
         print(f"Received message: {msg.data}")
         node_id = msg.data
         self.module_node_table[node_id] = True
-
-        # Publisher for testing logic
-        self.pub_global_heartbeat.publish(msg)
 
     def reset_module_node_table(self):
         """Resets the is_alive state of module nodes
         """
         for key in self.module_node_table:
-            self.module_node_table[key] = False
+            if self.module_node_table[key] is False:
+                self.health_state = key
+            else:
+                self.module_node_table[key] = False
+        self.pub_module_heartbeat()
 
     def local_safety_checks(self):
         """Design your local safety checks here.
+            Implement heath_state_errors
         """
+
 
 def main(args=None):
     """Main function
+        Publishes module health_state with initial value -1
+        to register node in Central Safety Node
     """
     rclpy.init(args=args)
 
     my_ros2_node = LocalSafety()
-    my_ros2_node.pub_module_heartbeat("OK")
+    my_ros2_node.pub_module_heartbeat()
 
     rclpy.spin(my_ros2_node)
 

9eed3f853700dd091bf0d2049a2d9d1bd61ff349
Daniel Gabler <daniel.gabler@student.uni-augsburg.de>
2025-06-25 09:51:16 +0000

    deleted dummy_node replaced with database_node, added getterService infrastructure, functionallity needs to be implemented once the database stands

---
+++ code/arlab_knowledge/arlab_knowledge/database_node.py
@@ -0,0 +1,134 @@
+from rclpy.callback_groups import ReentrantCallbackGroup
+from arlab_knowledge_interfaces.srv import GetEntities
+from arlab_knowledge_interfaces.srv import GetPoseAndReference
+from arlab_knowledge_interfaces.srv import GetShape
+from arlab_knowledge_interfaces.srv import DoorGetOpen
+from arlab_knowledge_interfaces.srv import DoorGetWidth
+from arlab_knowledge_interfaces.srv import GetDescription
+from arlab_knowledge_interfaces.msg import EntityType
+from arlab_knowledge_interfaces.msg import Result
+
+import rclpy
+from rclpy.node import Node
+
+
+class DatabaseNode(Node):
+    """This node provides a service that adds two ints
+
+    The custom service definition can be found in the
+    arlab_template_interfaces.srv folder
+    """
+
+    def __init__(self):
+        super().__init__(type(self).__name__)
+        entity_callback_group = ReentrantCallbackGroup()
+        shape_callback_group = ReentrantCallbackGroup()
+        door_open_callback_group = ReentrantCallbackGroup()
+        door_width_callback_group = ReentrantCallbackGroup()
+        pose_and_reference_callback_group = ReentrantCallbackGroup()
+        description_callback_group = ReentrantCallbackGroup()
+        self.srv = self.create_service(
+            GetEntities,
+            "/get_entities_callback",
+            self.get_entities_callback,
+            callback_group=entity_callback_group,
+        )
+
+        self.srv = self.create_service(
+            GetShape,
+            "/get_shape_callback",
+            self.get_shape_callback,
+            callback_group=shape_callback_group,
+        )
+
+        self.srv = self.create_service(
+            DoorGetOpen,
+            "/get_door_open_callback",
+            self.get_door_open_callback,
+            callback_group=door_open_callback_group,
+        )
+
+        self.srv = self.create_service(
+            DoorGetWidth,
+            "/get_door_width_callback",
+            self.get_door_width_callback,
+            callback_group=door_width_callback_group,
+        )
+
+        self.srv = self.create_service(
+            GetPoseAndReference,
+            "/get_pose_and_reference",
+            self.get_pose_and_reference,
+            callback_group=pose_and_reference_callback_group,
+        )
+
+        self.srv = self.create_service(
+            GetDescription,
+            "/get_description",
+            self.get_description,
+            callback_group=description_callback_group,
+        )
+
+    async def get_entities_callback(
+        self, request: GetEntities.Request, response: GetEntities.Response
+    ):
+        ### TODO implement the function once the database exists
+        # request an datenbank weiterleiten
+        # answer = antwort von datenbank bestehen aus EntitiyType und Result
+        answer = 1
+        if answer:
+            # response.entities = answer.EntityType
+            response.entities = 0
+        self.get_logger().info("Incoming request:")
+        return response
+
+    async def get_shape_callback(
+        self, request: GetEntities.Request, response: GetEntities.Response
+    ):
+        ### TODO implement the function once the database exists
+        return response
+
+    async def get_door_open_callback(
+        self, request: GetEntities.Request, response: GetEntities.Response
+    ):
+        ### TODO implement the function once the database exists
+        return response
+
+    async def get_door_width_callback(
+        self, request: GetEntities.Request, response: GetEntities.Response
+    ):
+        ### TODO implement the function once the database exists
+        return response
+
+    async def get_pose_and_reference(
+        self, request: GetEntities.Request, response: GetEntities.Response
+    ):
+        ### TODO implement the function once the database exists
+        return response
+
+    async def get_description(
+        self, request: GetEntities.Request, response: GetEntities.Response
+    ):
+        ### TODO implement the function once the database exists
+        return response
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    database_node = DatabaseNode()
+    try:
+        database_node.get_logger().info(
+            "Beginning database_node, shut down with CTRL-C"
+        )
+        rclpy.spin(database_node)
+    except KeyboardInterrupt:
+        database_node.get_logger().info(
+            "Keyboard interrupt, shutting down database_node.\n"
+        )
+    database_node.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == "__main__":
+    main()
--- code/arlab_knowledge/arlab_knowledge/dummy.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hi from arlab_knowledge.")
-
-
-if __name__ == "__main__":
-    main()

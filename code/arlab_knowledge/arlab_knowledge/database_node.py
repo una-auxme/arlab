4f3a5cd6098f018d189f11a55f8b56385c4a327d
Daniel Gabler <daniel.gabler@student.uni-augsburg.de>
2025-06-27 12:29:36 +0000

    addEntity service added

--- code/arlab_knowledge/arlab_knowledge/database_node.py
+++ code/arlab_knowledge/arlab_knowledge/database_node.py
@@ -5,11 +5,15 @@ from arlab_knowledge_interfaces.srv import GetShape
 from arlab_knowledge_interfaces.srv import DoorGetOpen
 from arlab_knowledge_interfaces.srv import DoorGetWidth
 from arlab_knowledge_interfaces.srv import GetDescription
+from arlab_knowledge_interfaces.srv import AddEntity
 from arlab_knowledge_interfaces.msg import EntityType
 from arlab_knowledge_interfaces.msg import Result
 
+
 import rclpy
 from rclpy.node import Node
+
+prefix = "/arlab"
 
 
 class DatabaseNode(Node):
@@ -21,52 +25,55 @@ class DatabaseNode(Node):
 
     def __init__(self):
         super().__init__(type(self).__name__)
-        entity_callback_group = ReentrantCallbackGroup()
-        shape_callback_group = ReentrantCallbackGroup()
-        door_open_callback_group = ReentrantCallbackGroup()
-        door_width_callback_group = ReentrantCallbackGroup()
-        pose_and_reference_callback_group = ReentrantCallbackGroup()
-        description_callback_group = ReentrantCallbackGroup()
+        self.reentrant_callback_group = ReentrantCallbackGroup()
+
         self.srv = self.create_service(
             GetEntities,
-            "/get_entities_callback",
+            f"{prefix}/get_entities_callback",
             self.get_entities_callback,
-            callback_group=entity_callback_group,
+            callback_group=self.reentrant_callback_group,
         )
 
         self.srv = self.create_service(
             GetShape,
             "/get_shape_callback",
             self.get_shape_callback,
-            callback_group=shape_callback_group,
+            callback_group=self.reentrant_callback_group,
         )
 
         self.srv = self.create_service(
             DoorGetOpen,
             "/get_door_open_callback",
             self.get_door_open_callback,
-            callback_group=door_open_callback_group,
+            callback_group=self.reentrant_callback_group,
         )
 
         self.srv = self.create_service(
             DoorGetWidth,
             "/get_door_width_callback",
             self.get_door_width_callback,
-            callback_group=door_width_callback_group,
+            callback_group=self.reentrant_callback_group,
         )
 
         self.srv = self.create_service(
             GetPoseAndReference,
-            "/get_pose_and_reference",
-            self.get_pose_and_reference,
-            callback_group=pose_and_reference_callback_group,
+            "/get_pose_and_reference_callback",
+            self.get_pose_and_reference_callback,
+            callback_group=self.reentrant_callback_group,
         )
 
         self.srv = self.create_service(
             GetDescription,
-            "/get_description",
-            self.get_description,
-            callback_group=description_callback_group,
+            "/get_description_callback",
+            self.get_description_callback,
+            callback_group=self.reentrant_callback_group,
+        )
+
+        self.srv = self.create_service(
+            AddEntity,
+            "/add_entity_callback ",
+            self.add_entity_callback,
+            callback_group=self.reentrant_callback_group,
         )
 
     async def get_entities_callback(
@@ -83,34 +90,41 @@ class DatabaseNode(Node):
         return response
 
     async def get_shape_callback(
-        self, request: GetEntities.Request, response: GetEntities.Response
+        self, request: GetShape.Request, response: GetShape.Response
     ):
         ### TODO implement the function once the database exists
         return response
 
     async def get_door_open_callback(
-        self, request: GetEntities.Request, response: GetEntities.Response
+        self, request: DoorGetOpen.Request, response: DoorGetOpen.Response
     ):
         ### TODO implement the function once the database exists
         return response
 
     async def get_door_width_callback(
-        self, request: GetEntities.Request, response: GetEntities.Response
+        self, request: DoorGetWidth.Request, response: DoorGetWidth.Response
     ):
         ### TODO implement the function once the database exists
         return response
 
-    async def get_pose_and_reference(
-        self, request: GetEntities.Request, response: GetEntities.Response
+    async def get_pose_and_reference_callback(
+        self,
+        request: GetPoseAndReference.Request,
+        response: GetPoseAndReference.Response,
     ):
         ### TODO implement the function once the database exists
         return response
 
-    async def get_description(
-        self, request: GetEntities.Request, response: GetEntities.Response
+    async def get_description_callback(
+        self, request: GetDescription.Request, response: GetDescription.Response
     ):
         ### TODO implement the function once the database exists
         return response
+
+    async def add_entity_callback(
+        self, request: AddEntity.Request, response: AddEntity.Response
+    ):
+        pass
 
 
 def main(args=None):

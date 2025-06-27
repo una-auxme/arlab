24734d009e2d6eaff4813b459e463d55edbf9e39
Peter Viechter <peter.viechter@student.uni-augsburg.de>
2025-06-27 13:19:53 +0000

    Open database connection

--- code/arlab_knowledge/arlab_knowledge/entities/shape.py
+++ code/arlab_knowledge/arlab_knowledge/entities/shape.py
@@ -8,13 +8,17 @@ from sqlalchemy.orm import (
     composite,
 )
 
-from . import Base, Entity
+from arlab_knowledge import Base
+
+import arlab_knowledge.entities.entity as entity_module
 
 
 class Shape(Base):
     __tablename__ = "shape"
 
-    id: Mapped[int] = mapped_column(primary_key=True)
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     entity_id: Mapped[int] = mapped_column(ForeignKey("entity.id"), unique=True)
 
-    entity: Mapped[Entity] = relationship(back_populates="shape", single_parent=True)
+    entity: Mapped["entity_module.Entity"] = relationship(
+        back_populates="shape", single_parent=True
+    )

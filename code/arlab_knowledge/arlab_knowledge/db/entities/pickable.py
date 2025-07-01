6077335e0156e84eb5e98c0f2e348b3f43909741
Peter Viechter <peter.viechter@student.uni-augsburg.de>
2025-07-01 08:51:03 +0000

    Add remaining entity classes and relations

--- code/arlab_knowledge/arlab_knowledge/db/entities/pickable.py
+++ code/arlab_knowledge/arlab_knowledge/db/entities/pickable.py
@@ -1,13 +1,24 @@
+from typing import Optional
+
 from .entity import Entity
 
 from sqlalchemy import ForeignKey
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 
 class Pickable(Entity):
-    __tablename__ = "pickable"
+    __tablename__ = "entity_pickable"
     id: Mapped[int] = mapped_column(ForeignKey("entity.id"), primary_key=True)
 
+    located_on_id: Mapped[Optional[int]] = mapped_column(
+        ForeignKey("entity_furniture.id", ondelete="CASCADE")
+    )
+    located_on: Mapped[Optional["Furniture"]] = relationship(  # type: ignore # noqa: F821
+        back_populates="pickables"
+    )
+
+    max_picking_force: Mapped[float]
+
     __mapper_args__ = {
-        "polymorphic_identity": "pickable",
+        "polymorphic_identity": "entity_pickable",
     }

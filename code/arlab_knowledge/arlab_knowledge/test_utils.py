de18787ebf2bc6351e462407d4d53244e03d27d6
Peter Viechter <peter.viechter@student.uni-augsburg.de>
2025-07-09 09:49:00 +0000

    Status tests

--- code/arlab_knowledge/arlab_knowledge/test_utils.py
+++ code/arlab_knowledge/arlab_knowledge/test_utils.py
@@ -1,3 +1,7 @@
+from builtin_interfaces.msg import Time
+from geometry_msgs.msg import Point, Pose, Quaternion
+from sqlalchemy import inspect
+
 from arlab_knowledge.db.base import Base
 from arlab_knowledge.db.entities.entity import Entity
 from arlab_knowledge.db.entities.furniture import (
@@ -11,9 +15,13 @@ from arlab_knowledge.db.entities.human import Human
 from arlab_knowledge.db.entities.pickable import Pickable
 from arlab_knowledge.db.ros_adapters.pose import PoseData
 from arlab_knowledge.db.ros_adapters.time import TimeData
-from builtin_interfaces.msg import Time
-from geometry_msgs.msg import Point, Pose, Quaternion
-from sqlalchemy import inspect
+from arlab_knowledge.db.status import (
+    ManipulationStatus,
+    MovementStatus,
+    RobotStatus,
+    RobotStatusEvent,
+    SafetyStatus,
+)
 
 
 def equality_check(b0: Base, b1: Base):
@@ -23,7 +31,10 @@ def equality_check(b0: Base, b1: Base):
         name = col.name
         value0 = getattr(b0, name)
         value1 = getattr(b1, name)
-        assert value0 == value1, f"{name} equality: {value0}, {value1}"
+        if isinstance(value0, Base) and isinstance(value1, Base):
+            equality_check(value0, value1)
+        else:
+            assert value0 == value1, f"{name} equality: {value0}, {value1}"
 
 
 def create_pose():
@@ -140,3 +151,25 @@ def get_table():
         stamp=TimeData(stamp),
         height=1.5,
     )
+
+
+def get_robot_status_event():
+    stamp = create_stamp()
+    status = get_manipulation_status()
+    return RobotStatusEvent(stamp=TimeData(stamp), sender="TestSender", status=status)
+
+
+def get_robot_status():
+    return RobotStatus(is_ok=True)
+
+
+def get_manipulation_status():
+    return ManipulationStatus(is_ok=True)
+
+
+def get_movement_status():
+    return MovementStatus(is_ok=True)
+
+
+def get_safety_status():
+    return SafetyStatus(is_ok=True)

89ffd779cd4d90eba8d01f8d961d554fbe0aa377
Peter Viechter <peter.viechter@student.uni-augsburg.de>
2025-07-09 09:24:11 +0000

    Rework Status architecture

--- code/arlab_knowledge/arlab_knowledge/db/status.py
+++ code/arlab_knowledge/arlab_knowledge/db/status.py
@@ -1,40 +1,157 @@
-from typing import Optional
+from typing import Any, Dict
 
-from sqlalchemy import Float, ForeignKey
+import rclpy.logging
+from arlab_knowledge_interfaces import msg
+from sqlalchemy import Float, ForeignKey, String
 from sqlalchemy.orm import Mapped, composite, mapped_column, relationship
+
+import arlab_knowledge.db as db
 
 from .base import Base
 from .ros_adapters.time import TimeData
 
 
-class MovementStatus(Base):
-    __tablename__ = "robot_movement_status"
-    id: Mapped[int] = mapped_column(
-        ForeignKey("robot_status_event.id", ondelete="CASCADE"), primary_key=True
+def status_msg_type_to_class(msg_type: msg.StatusType):
+    if msg_type.id == msg.StatusType.STATUS:
+        return RobotStatus
+    if msg_type.id == msg.StatusType.STATUS_MANIPULATION:
+        return ManipulationStatus
+    if msg_type.id == msg.StatusType.STATUS_MOVEMENT:
+        return MovementStatus
+    if msg_type.id == msg.StatusType.STATUS_SAFETY:
+        return SafetyStatus
+    else:
+        return None
+
+
+def status_extract_type_msg(e: "RobotStatus") -> msg.StatusType:
+    m = msg.StatusType()
+    m.id = msg.StatusType.STATUS
+    if isinstance(e, ManipulationStatus):
+        m.id = msg.StatusType.STATUS_MANIPULATION
+    if isinstance(e, MovementStatus):
+        m.id = msg.StatusType.STATUS_MOVEMENT
+    if isinstance(e, SafetyStatus):
+        m.id = msg.StatusType.STATUS_SAFETY
+    return m
+
+
+class RobotStatus(Base):
+    __tablename__ = "robot_status"
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    type: Mapped[str]
+
+    event: Mapped["RobotStatusEvent"] = relationship(
+        back_populates="status", cascade="all, delete"
     )
-    event: Mapped["RobotStatusEvent"] = relationship(back_populates="movement")
 
     is_ok: Mapped[bool]
 
+    __mapper_args__ = {
+        "polymorphic_identity": "robot_status",
+        "polymorphic_on": "type",
+    }
 
-class ManipulationStatus(Base):
-    __tablename__ = "robot_manipulation_status"
+    @classmethod
+    def from_ros_msg(cls, m: msg.RobotStatus) -> "RobotStatus":
+        """Creates a RobotStatus from m
+
+        Note that the returned status might be a subclass of RobotStatus
+        """
+        status_type = status_msg_type_to_class(m.status_type)
+        if status_type is None:
+            rclpy.logging.get_logger(db.DB_LOGGER_NAME).error(
+                f"Received entity type '{m.status_type}' is not supported."
+                f"Base class 'Entity' will be used instead."
+            )
+            status_type = RobotStatus
+
+        kwargs = status_type._extract_kwargs(m)
+
+        return status_type(**kwargs)
+
+    @classmethod
+    def _extract_kwargs(cls, m: msg.RobotStatus) -> Dict[str, Any]:
+        """Extracts all attributes for the __init__ of this type from m
+
+        Args:
+            m (msg.RobotStatus): ROS message to extract data from
+
+        Returns:
+            Dict: arguments for the __init__ function
+        """
+        return {"is_ok": m.is_ok}
+
+    def to_ros_msg(self) -> msg.RobotStatus:
+        return msg.RobotStatus(
+            status_type=status_extract_type_msg(self), is_ok=self.is_ok
+        )
+
+
+class MovementStatus(RobotStatus):
+    __tablename__ = "robot_status_movement"
     id: Mapped[int] = mapped_column(
-        ForeignKey("robot_status_event.id", ondelete="CASCADE"), primary_key=True
+        ForeignKey("robot_status.id", ondelete="CASCADE"), primary_key=True
     )
-    event: Mapped["RobotStatusEvent"] = relationship(back_populates="manipulation")
 
-    is_ok: Mapped[bool]
+    __mapper_args__ = {
+        "polymorphic_identity": "robot_status_movement",
+    }
+
+    @classmethod
+    def _extract_kwargs(cls, m: msg.RobotStatus) -> Dict:
+        kwargs = super(cls, cls)._extract_kwargs(m)
+        # Assign subclass specific attributes here
+        return kwargs
+
+    def to_ros_msg(self) -> msg.RobotStatus:
+        m = super().to_ros_msg()
+        # Assign subclass specific attributes here
+        return m
 
 
-class SafetyStatus(Base):
-    __tablename__ = "robot_safety_status"
+class ManipulationStatus(RobotStatus):
+    __tablename__ = "robot_status_manipulation"
     id: Mapped[int] = mapped_column(
-        ForeignKey("robot_status_event.id", ondelete="CASCADE"), primary_key=True
+        ForeignKey("robot_status.id", ondelete="CASCADE"), primary_key=True
     )
-    event: Mapped["RobotStatusEvent"] = relationship(back_populates="safety")
 
-    is_ok: Mapped[bool]
+    __mapper_args__ = {
+        "polymorphic_identity": "robot_status_manipulation",
+    }
+
+    @classmethod
+    def _extract_kwargs(cls, m: msg.RobotStatus) -> Dict:
+        kwargs = super(cls, cls)._extract_kwargs(m)
+        # Assign subclass specific attributes here
+        return kwargs
+
+    def to_ros_msg(self) -> msg.RobotStatus:
+        m = super().to_ros_msg()
+        # Assign subclass specific attributes here
+        return m
+
+
+class SafetyStatus(RobotStatus):
+    __tablename__ = "robot_status_safety"
+    id: Mapped[int] = mapped_column(
+        ForeignKey("robot_status.id", ondelete="CASCADE"), primary_key=True
+    )
+
+    __mapper_args__ = {
+        "polymorphic_identity": "robot_status_safety",
+    }
+
+    @classmethod
+    def _extract_kwargs(cls, m: msg.RobotStatus) -> Dict:
+        kwargs = super(cls, cls)._extract_kwargs(m)
+        # Assign subclass specific attributes here
+        return kwargs
+
+    def to_ros_msg(self) -> msg.RobotStatus:
+        m = super().to_ros_msg()
+        # Assign subclass specific attributes here
+        return m
 
 
 class RobotStatusEvent(Base):
@@ -47,12 +164,23 @@ class RobotStatusEvent(Base):
         mapped_column("stamp_sec", Float),
     )
 
-    movement: Mapped[Optional["MovementStatus"]] = relationship(
+    sender: Mapped[str] = mapped_column(String(100))
+
+    status_id: Mapped[int] = mapped_column(
+        ForeignKey("robot_status.id", ondelete="CASCADE")
+    )
+    status: Mapped["RobotStatus"] = relationship(
         back_populates="event", cascade="all, delete", single_parent=True
     )
-    manipulation: Mapped[Optional["ManipulationStatus"]] = relationship(
-        back_populates="event", cascade="all, delete", single_parent=True
-    )
-    safety: Mapped[Optional["SafetyStatus"]] = relationship(
-        back_populates="event", cascade="all, delete", single_parent=True
-    )
+
+    @classmethod
+    def from_ros_msg(cls, m: msg.RobotStatusEvent) -> "RobotStatusEvent":
+        """Creates a status event from m"""
+        status = RobotStatus.from_ros_msg(m.status)
+        return cls(stamp=TimeData(m.stamp), sender=m.sender, status=status)
+
+    def to_ros_msg(self) -> msg.RobotStatusEvent:
+        status = self.status.to_ros_msg()
+        return msg.RobotStatusEvent(
+            stamp=self.stamp.time, sender=self.sender, status=status
+        )

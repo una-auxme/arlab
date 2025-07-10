853073502182b2737b130fdd6b2be807377275ad
Aleksander Michalak <aleksander1.michalak@uni-a.de>
2025-07-10 17:21:12 +0200

    Implemented multi heartbeat and updated logic.

--- code/arlab_safety/arlab_safety/central_safety_node.py
+++ code/arlab_safety/arlab_safety/central_safety_node.py
@@ -1,4 +1,6 @@
 import rclpy
+import rospy
+
 from rclpy.node import Node
 from std_msgs.msg import Int32MultiArray
 
@@ -9,10 +11,18 @@ class CentralSafetyNode(Node):
     def __init__(self):
         super().__init__(type(self).__name__)
 
-        # {int:module_id: id/state: error_state}
+        # module safety table
+        # {
+        #   int:module_id:
+        #       {
+        #           error_state: INT,
+        #           "last_seen": Timestamp,
+        #           "heartbeat": FLOAT
+        #       }
+        # }
         self.module_safety_table = {}
 
-        self.timer = self.create_timer(1.0, self.reset_module_safety_table)
+        self.timer = self.create_timer(0.001, self.reset_module_safety_table)
         self.create_subscription(
             Int32MultiArray, "/global_heartbeat", self.callback, 10
         )
@@ -39,20 +49,23 @@ class CentralSafetyNode(Node):
         """Adds module to its safety list."""
         if not self.module_safety_table.keys().__contains__(module_id):
             print(f"Registering module {module_id} now.")
-            self.module_safety_table[module_id] = -1
+            self.module_safety_table[module_id]["error_state"] = -1
+            self.module_safety_table[module_id]["last_seen"] = rospy.get_time()
+            self.module_safety_table[module_id]["heartbeat"] = 0.1
         else:
-            print(f"Modul {module_id} does not share its health status!")
+            print(f"Modul {module_id} is stuck in registering to global health node!")
             self.critical_error(module_id, -1)
 
     def module_working(self, module_id):
         """Updates working state in the module table."""
         self.module_safety_table[module_id] = 0
+        self.module_safety_table[module_id]["last_seen"] = rospy.get_time()
 
     def critical_error(self, module_id, module_state):
         """Handles the safety guidelines
         for specifc errors states.
         """
-        self.module_safety_table[module_id] = module_state
+        self.module_safety_table[module_id]["error_state"] = module_state
         if module_state == -1:
             pass  # Handle module does not share health state
         else:
@@ -61,12 +74,13 @@ class CentralSafetyNode(Node):
 
     def reset_module_safety_table(self):
         """Resets the error_state of modules"""
-        for module_id, error_state in self.module_safety_table.items():
-            if error_state > 0:
-                # Error handling here
-                print(f"Modul {module_id} hat einen Fehlercode {error_state}!")
-            else:
-                self.module_safety_table[module_id] = -1
+        for module_id, (
+            _,
+            last_seen,
+            heartbeat,
+        ) in self.module_safety_table.items():
+            if rospy.get_time() - last_seen > heartbeat:
+                self.critical_error(module_id, -1)
 
     def system_freeze(self):
         """In a worst case scenario this function

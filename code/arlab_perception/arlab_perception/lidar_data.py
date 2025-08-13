57b49ce40cbd24a88cdf8bf8ebfdab3794de82bf
Meruna <meruna.yugarajah@uni-a.de>
2025-08-13 17:50:04 +0200

    Solve linter issue

--- code/arlab_perception/arlab_perception/lidar_data.py
+++ code/arlab_perception/arlab_perception/lidar_data.py
@@ -3,14 +3,14 @@ from rclpy.node import Node
 from sensor_msgs.msg import LaserScan
 
 
-
 class LidarPubSub(Node):
     def __init__(self):
         super().__init__(type(self).__name__)
 
-
         # Abonniert das LIDAR-Topic
-        self.lidar_sub = self.create_subscription(LaserScan, "/scan", self.lidar_callback, 10)
+        self.lidar_sub = self.create_subscription(
+            LaserScan, "/scan", self.lidar_callback, 10
+        )
 
         # Wird benötigt, um Garbage Collection zu verhindern
         self.lidar_pub = self.create_publisher(LaserScan, "/relay_scan", 10)
@@ -18,6 +18,7 @@ class LidarPubSub(Node):
     def lidar_callback(self, msg: LaserScan):
         # Es wird einfach weitergeleitet, ohne Filterung
         self.lidar_pub.publish(msg)
+
 
 def main(args=None):
     rclpy.init(args=args)

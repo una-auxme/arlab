8fd78e7d043a550773d677ed7b5c7b9f253bfad0
Peter Viechter <peter.viechter@student.uni-augsburg.de>
2025-09-11 15:30:49 +0000

    Add docu to database_node callbacks

--- code/arlab_knowledge/arlab_knowledge/database_node.py
+++ code/arlab_knowledge/arlab_knowledge/database_node.py
@@ -111,6 +111,12 @@ class DatabaseNode(Node):
         self.reentrant_callback_group = ReentrantCallbackGroup()
 
     async def async_init(self):
+        """Async init function
+
+        Needs to be called after the node has been created
+
+        Initializes the database schema and services
+        """
         async with self.db_engine.begin() as conn:
             await conn.run_sync(Base.metadata.create_all)
         self._init_services()
@@ -291,6 +297,18 @@ class DatabaseNode(Node):
     async def get_entities_callback(
         self, request: GetEntities.Request, response: GetEntities.Response
     ):
+        """Callback for the GetEntities service
+
+        Available at {prefix}/get_entities
+
+        Gets entities by type
+
+        Look at the arlab_knowledge_interfaces/srv/GetEntities.srv for more info
+
+        Args:
+            request: Request containing the class of the entities to get
+            response: Response object to populate with result/error data
+        """
         async with self.Session(response) as session:
             entity_class = entities.entity_msg_type_to_class(request.entity_type)
 
@@ -315,6 +333,18 @@ class DatabaseNode(Node):
     async def del_entities_callback(
         self, request: DelEntities.Request, response: DelEntities.Response
     ):
+        """Callback for the DelEntities service
+
+        Available at {prefix}/del_entities
+
+        Deletes entities by their IDs
+
+        Look at the arlab_knowledge_interfaces/srv/DelEntities.srv for more info
+
+        Args:
+            request: Request containing entity IDs to delete
+            response: Response object to populate with result/error data
+        """
         async with self.Session(response) as session:
             entity_ids = request.entityids
             if not entity_ids:
@@ -331,6 +361,18 @@ class DatabaseNode(Node):
     async def get_entity_callback(
         self, request: GetEntity.Request, response: GetEntity.Response
     ):
+        """Callback for the GetEntity service
+
+        Available at {prefix}/get_entity
+
+        Retrieves entity data by ID
+
+        Look at the arlab_knowledge_interfaces/srv/GetEntity.srv for more info
+
+        Args:
+            request: Request containing entity ID to retrieve
+            response: Response object to populate with result/error data
+        """
         async with self.Session(response) as session:
             entity = await session.get(Entity, request.entityid)
             if entity is None:
@@ -342,6 +384,18 @@ class DatabaseNode(Node):
     async def get_shape_callback(
         self, request: GetShape.Request, response: GetShape.Response
     ):
+        """Callback for the GetShape service
+
+        Available at {prefix}/get_shape
+
+        Retrieves shape data for an entity by ID
+
+        Look at the arlab_knowledge_interfaces/srv/GetShape.srv for more info
+
+        Args:
+            request: Request containing entity ID to retrieve shape for
+            response: Response object to populate with result/error data
+        """
         async with self.Session(response) as session:
             stmt = (
                 select(Shape)
@@ -371,6 +425,18 @@ class DatabaseNode(Node):
         request: GetPose.Request,
         response: GetPose.Response,
     ):
+        """Callback for the GetPose service
+
+        Available at {prefix}/get_pose
+
+        Retrieves pose data for an entity by ID
+
+        Look at the arlab_knowledge_interfaces/srv/GetPose.srv for more info
+
+        Args:
+            request: Request containing entity ID to retrieve pose for
+            response: Response object to populate with result/error data
+        """
         async with self.Session(response) as session:
             stmt = select(Entity.pose).where(Entity.id == request.entityid)
             result = await session.execute(stmt)
@@ -384,6 +450,18 @@ class DatabaseNode(Node):
     async def get_description_callback(
         self, request: GetDescription.Request, response: GetDescription.Response
     ):
+        """Callback for the GetDescription service
+
+        Available at {prefix}/get_description
+
+        Retrieves description data for an entity by ID
+
+        Look at the arlab_knowledge_interfaces/srv/GetDescription.srv for more info
+
+        Args:
+            request: Request containing entity ID to retrieve description for
+            response: Response object to populate with result/error data
+        """
         async with self.Session(response) as session:
             stmt = select(Entity.description).where(Entity.id == request.entityid)
             result = await session.execute(stmt)
@@ -397,6 +475,18 @@ class DatabaseNode(Node):
     async def add_entity_callback(
         self, request: AddEntity.Request, response: AddEntity.Response
     ):
+        """Callback for the AddEntity service
+
+        Available at {prefix}/add_entity
+
+        Adds a new entity to the database
+
+        Look at the arlab_knowledge_interfaces/srv/AddEntity.srv for more info
+
+        Args:
+            request: Request containing entity data to add
+            response: Response object to populate with result/error data
+        """
         async with self.Session(response) as session:
             entity = Entity.from_ros_msg(request.data)
             async with session.begin():
@@ -407,6 +497,18 @@ class DatabaseNode(Node):
     async def update_entity_callback(
         self, request: UpdEntity.Request, response: UpdEntity.Response
     ):
+        """Callback for the UpdEntity service
+
+        Available at {prefix}/upd_entity
+
+        Updates an existing entity with new data
+
+        Look at the arlab_knowledge_interfaces/srv/UpdEntity.srv for more info
+
+        Args:
+            request: Request containing entity ID and data to update
+            response: Response object to populate with result/error data
+        """
         async with self.Session(response) as session:
             entity = await session.get(Entity, request.entityid)
             if entity is None:
@@ -427,6 +529,18 @@ class DatabaseNode(Node):
     async def update_pose_callback(
         self, request: UpdPose.Request, response: UpdPose.Response
     ):
+        """Callback for the UpdPose service
+
+        Available at {prefix}/upd_pose
+
+        Updates the pose of an existing entity
+
+        Look at the arlab_knowledge_interfaces/srv/UpdPose.srv for more info
+
+        Args:
+            request: Request containing entity ID and new pose data
+            response: Response object to populate with result/error data
+        """
         async with self.Session(response) as session:
             entity = await session.get(Entity, request.entityid)
             if entity is None:
@@ -441,6 +555,18 @@ class DatabaseNode(Node):
     async def update_shape_callback(
         self, request: UpdShape.Request, response: UpdShape.Response
     ):
+        """Callback for the UpdShape service
+
+        Available at {prefix}/upd_shape
+
+        Updates the shape of an existing entity
+
+        Look at the arlab_knowledge_interfaces/srv/UpdShape.srv for more info
+
+        Args:
+            request: Request containing entity ID and new shape data
+            response: Response object to populate with result/error data
+        """
         async with self.Session(response) as session:
             entity_shape = await session.get(
                 Entity, request.entityid, options=(joinedload(Entity.shape),)
@@ -456,6 +582,18 @@ class DatabaseNode(Node):
     async def furniture_update_pickable_callback(
         self, request: UpdReference.Request, response: UpdReference.Response
     ):
+        """Callback for the UpdReference service
+
+        Available at {prefix}/furniture_update_pickable
+
+        Updates the reference between furniture and pickable objects
+
+        Look at the arlab_knowledge_interfaces/srv/UpdReference.srv for more info
+
+        Args:
+            request: Request containing parent ID, child ID, and delete flag
+            response: Response object to populate with result/error data
+        """
         async with self.Session(response) as session:
             furniture_id = request.parentid
             furniture = await session.execute(
@@ -492,6 +630,18 @@ class DatabaseNode(Node):
     async def furniture_get_pickable_callback(
         self, request: GetReference.Request, response: GetReference.Response
     ):
+        """Callback for the GetReference service
+
+        Available at {prefix}/furniture_get_pickable
+
+        Retrieves pickable objects associated with a furniture entity
+
+        Look at the arlab_knowledge_interfaces/srv/GetReference.srv for more info
+
+        Args:
+            request: Request containing entity ID of furniture
+            response: Response object to populate with result/error data
+        """
         async with self.Session(response) as session:
             furniture = await session.execute(
                 select(Furniture)
@@ -510,6 +660,18 @@ class DatabaseNode(Node):
     async def pickable_get_furniture_callback(
         self, request: GetReference.Request, response: GetReference.Response
     ):
+        """Callback for the GetReference service
+
+        Available at {prefix}/pickable_get_furniture
+
+        Retrieves the furniture a pickable object is located on
+
+        Look at the arlab_knowledge_interfaces/srv/GetReference.srv for more info
+
+        Args:
+            request: Request containing entity ID of pickable
+            response: Response object to populate with result/error data
+        """
         async with self.Session(response) as session:
             pickable = await session.execute(
                 select(Pickable)
@@ -520,12 +682,24 @@ class DatabaseNode(Node):
             if pickable is None:
                 response.result.result_type = Result.ERROR_ID_NOT_FOUND
                 return response
-        response.entities = pickable.located_on_id
+            response.entities = [pickable.located_on_id]
         return response
 
     async def cupboard_get_shelf_callback(
         self, request: GetReference.Request, response: GetReference.Response
     ):
+        """Callback for the GetReference service
+
+        Available at {prefix}/cupboard_get_shelf
+
+        Retrieves shelves associated with a cupboard entity
+
+        Look at the arlab_knowledge_interfaces/srv/GetReference.srv for more info
+
+        Args:
+            request: Request containing entity ID of cupboard
+            response: Response object to populate with result/error data
+        """
         async with self.Session(response) as session:
             cupboard = await session.execute(
                 select(Cupboard)
@@ -536,12 +710,24 @@ class DatabaseNode(Node):
             if cupboard is None:
                 response.result.result_type = Result.ERROR_ID_NOT_FOUND
                 return response
-        response.entities = list(map(lambda shelf: shelf.id, cupboard.shelves))
+            response.entities = list(map(lambda shelf: shelf.id, cupboard.shelves))
         return response
 
     async def shelf_get_cupboard_callback(
         self, request: GetReference.Request, response: GetReference.Response
     ):
+        """Callback for the GetReference service
+
+        Available at {prefix}/shelf_get_cupboard
+
+        Retrieves the cupboard associated with a shelf entity
+
+        Look at the arlab_knowledge_interfaces/srv/GetReference.srv for more info
+
+        Args:
+            request: Request containing entity ID of shelf
+            response: Response object to populate with result/error data
+        """
         async with self.Session(response) as session:
             shelf = await session.execute(
                 select(Shelf)
@@ -552,12 +738,24 @@ class DatabaseNode(Node):
             if shelf is None:
                 response.result.result_type = Result.ERROR_ID_NOT_FOUND
                 return response
-        response.entities = shelf.cupboard_id
+            response.entities = [shelf.cupboard_id]
         return response
 
     async def add_map_callback(
         self, request: AddMap.Request, response: AddMap.Response
     ):
+        """Callback for the AddMap service
+
+        Available at {prefix}/add_map
+
+        Adds a new map to the database
+
+        Look at the arlab_knowledge_interfaces/srv/AddMap.srv for more info
+
+        Args:
+            request: Request containing map data to add
+            response: Response object to populate with result/error data
+        """
         async with self.Session(response) as session:
             map = Map(grid=OccupancyGridData(request.grid))
             async with session.begin():
@@ -568,6 +766,18 @@ class DatabaseNode(Node):
     async def get_map_callback(
         self, request: GetMap.Request, response: GetMap.Response
     ):
+        """Callback for the GetMap service
+
+        Available at {prefix}/get_map
+
+        Retrieves a map based on age filters and offset index
+
+        Look at the arlab_knowledge_interfaces/srv/GetMap.srv for more info
+
+        Args:
+            request: Request containing age filters and offset index
+            response: Response object to populate with map data
+        """
         async with self.Session(response) as session:
             map = await session.execute(
                 select(Map)
@@ -604,6 +814,18 @@ class DatabaseNode(Node):
     async def add_status_event_callback(
         self, request: AddStatusEvent.Request, response: AddStatusEvent.Response
     ):
+        """Callback for the AddStatusEvent service
+
+        Available at {prefix}/add_status_event
+
+        Adds a new status event to the database
+
+        Look at the arlab_knowledge_interfaces/srv/AddStatusEvent.srv for more info
+
+        Args:
+            request: Request containing status event data to add
+            response: Response object to populate with result/error data
+        """
         async with self.Session(response) as session:
             event = RobotStatusEvent.from_ros_msg(request.event)
             async with session.begin():
@@ -618,6 +840,18 @@ class DatabaseNode(Node):
     async def get_status_events_callback(
         self, request: GetStatusEvents.Request, response: GetStatusEvents.Response
     ):
+        """Callback for the GetStatusEvents service
+
+        Available at {prefix}/get_status_events
+
+        Retrieves status events filtered by age and ordered by timestamp
+
+        Look at the arlab_knowledge_interfaces/srv/GetStatusEvents.srv for more info
+
+        Args:
+            request: Request containing status type, age filters, and offset index
+            response: Response object to populate with status events data
+        """
         async with self.Session(response) as session:
             status_class = status.status_msg_type_to_class(request.status_type)
 
--- code/arlab_knowledge/setup.py
+++ code/arlab_knowledge/setup.py
@@ -4,7 +4,7 @@ package_name = "arlab_knowledge"
 
 setup(
     name=package_name,
-    version="0.0.0",
+    version="0.1.0",
     packages=find_packages(exclude=["test"]),
     data_files=[
         ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
@@ -12,9 +12,9 @@ setup(
     ],
     install_requires=["setuptools"],
     zip_safe=True,
-    maintainer="root",
-    maintainer_email="root@todo.todo",
-    description="TODO: Package description",
+    maintainer="Peter Viechter, Daniel Gabler",
+    maintainer_email="peter.viechter@uni-augsburg.de",
+    description="Arlab knowledge base: database for handling the robot's surroundings, events and maps",
     license="MIT",
     tests_require=["pytest"],
     entry_points={

88c83b465b4cc93922686a4cfde0965db729d0be
Schmieo <leonie.schmidt2000@outlook.de>
2025-09-21 21:11:37 +0200

    Update setup.py

--- code/arlab_calibration/arlab_calibration/setup.py
+++ code/arlab_calibration/arlab_calibration/setup.py
@@ -16,7 +16,7 @@ setup(
     maintainer='sofia',
     maintainer_email='sofia.oettl@gmail.com',
     description='TODO: Package description',
-    license='TODO: License declaration',
+    license='MIT',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [

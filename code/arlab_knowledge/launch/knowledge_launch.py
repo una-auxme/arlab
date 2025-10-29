8917e1c64fedce37d825ad0b5cb0e63a7387fe0a
Daniel Gabler <daniel.gabler@student.uni-augsburg.de>
2025-10-29 16:46:16 +0100

    linter fixes

--- code/arlab_knowledge/launch/knowledge_launch.py
+++ code/arlab_knowledge/launch/knowledge_launch.py
@@ -3,17 +3,19 @@ from launch_ros.actions import Node
 
 
 def generate_launch_description():
-    return LaunchDescription([
-        Node(
-            package='arlab_knowledge',
-            namespace='arlab_knowledge',
-            executable='knowledge_visualization',
-            name='knowledge_visualization'
-        ),
-        Node(
-            package='arlab_knowledge',
-            namespace='arlab_knowledge',
-            executable='database_node',
-            name='database_node'
-        ),
-    ])
+    return LaunchDescription(
+        [
+            Node(
+                package="arlab_knowledge",
+                namespace="arlab_knowledge",
+                executable="knowledge_visualization",
+                name="knowledge_visualization",
+            ),
+            Node(
+                package="arlab_knowledge",
+                namespace="arlab_knowledge",
+                executable="database_node",
+                name="database_node",
+            ),
+        ]
+    )

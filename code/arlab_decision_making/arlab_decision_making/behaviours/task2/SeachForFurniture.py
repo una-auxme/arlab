e481d8de4e6f30adab8a66dcd103357c49a16bf2
Daniel <daniel.gabler@student.uni-augsburg.de>
2025-11-03 11:51:13 +0000

    python sceleton for task 2 added

---
+++ code/arlab_decision_making/arlab_decision_making/behaviours/task2/SeachForFurniture.py
@@ -0,0 +1,12 @@
+from py_trees.behaviour import Behaviour
+from py_trees.common import Status
+
+
+def get_tree() -> Behaviour:
+    raise NotImplementedError()
+
+
+class SearchForFurniture(Behaviour):
+    def update(self):
+        self.feedback_message = "not implemented"
+        return Status.FAILURE

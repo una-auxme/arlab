e395f2a955bba919edbb8f5c398ce1381e2478e7
Peter Viechter <peter.viechter@student.uni-augsburg.de>
2025-11-24 15:03:49 +0000

    Working tts node
    Co-authored-by: Daniel Gabler <daniel.gabler@student.uni-augsburg.de>

---
+++ code/arlab_speech_controller/arlab_speech_controller/moshi_tts_gen.py
@@ -0,0 +1,119 @@
+# Based on https://github.com/kyutai-labs/delayed-streams-modeling/blob/8fb54b2b07a34a852e148ad11b820e7526c48b59/scripts/tts_pytorch_streaming.py
+
+import typing as tp
+from dataclasses import dataclass
+
+import torch
+from moshi.conditioners import dropout_all_conditions
+from moshi.models.lm import LMGen
+from moshi.models.tts import (
+    ConditionAttributes,
+    TTSModel,
+)
+
+
+def _make_null(
+    all_attributes: tp.Sequence[ConditionAttributes],
+) -> list[ConditionAttributes]:
+    # When using CFG, returns the null conditions.
+    return dropout_all_conditions(all_attributes)
+
+
+@dataclass
+class TTSGen:
+    tts_model: TTSModel
+    attributes: tp.Sequence[ConditionAttributes]
+    on_frame: tp.Optional[tp.Callable[[torch.Tensor], None]] = None
+
+    def __post_init__(self):
+        tts_model = self.tts_model
+        attributes = self.attributes
+        self.offset = 0
+        self.state = self.tts_model.machine.new_state([])
+        if tts_model.cfg_coef != 1.0:
+            if tts_model.valid_cfg_conditionings:
+                raise ValueError(
+                    "This model does not support direct CFG, but was trained with "
+                    "CFG distillation. "
+                    "Pass instead `cfg_coef` to `make_condition_attributes`."
+                )
+            nulled = _make_null(attributes)
+            attributes = list(attributes) + nulled
+
+        assert tts_model.lm.condition_provider is not None
+        prepared = tts_model.lm.condition_provider.prepare(attributes)
+        condition_tensors = tts_model.lm.condition_provider(prepared)
+
+        def _on_text_logits_hook(text_logits):
+            if tts_model.padding_bonus:
+                text_logits[..., tts_model.machine.token_ids.pad] += (
+                    tts_model.padding_bonus
+                )
+            return text_logits
+
+        def _on_audio_hook(audio_tokens):
+            audio_offset = tts_model.lm.audio_offset
+            delays = tts_model.lm.delays
+            for q in range(audio_tokens.shape[1]):
+                delay = delays[q + audio_offset]
+                if self.offset < delay + tts_model.delay_steps:
+                    audio_tokens[:, q] = tts_model.machine.token_ids.zero
+
+        def _on_text_hook(text_tokens):
+            tokens = text_tokens.tolist()
+            out_tokens = []
+            for token in tokens:
+                out_token, _ = tts_model.machine.process(self.offset, self.state, token)
+                out_tokens.append(out_token)
+            text_tokens[:] = torch.tensor(
+                out_tokens, dtype=torch.long, device=text_tokens.device
+            )
+
+        tts_model.lm.dep_q = tts_model.n_q
+        self.lm_gen = LMGen(
+            tts_model.lm,
+            temp=tts_model.temp,
+            temp_text=tts_model.temp,
+            cfg_coef=tts_model.cfg_coef,
+            condition_tensors=condition_tensors,
+            on_text_logits_hook=_on_text_logits_hook,
+            on_text_hook=_on_text_hook,
+            on_audio_hook=_on_audio_hook,
+            cfg_is_masked_until=None,
+            cfg_is_no_text=True,
+        )
+        self.lm_gen.streaming_forever(1)
+
+    def reset_state(self):
+        self.state = self.tts_model.machine.new_state([])
+        self.offset = 0
+
+    def process_last(self):
+        while len(self.state.entries) > 0 or self.state.end_step is not None:
+            self._step()
+        additional_steps = (
+            self.tts_model.delay_steps + max(self.tts_model.lm.delays) + 8
+        )
+        for _ in range(additional_steps):
+            self._step()
+
+    def process(self):
+        while len(self.state.entries) > self.tts_model.machine.second_stream_ahead:
+            self._step()
+
+    def _step(self):
+        missing = self.tts_model.lm.n_q - self.tts_model.lm.dep_q
+        input_tokens = torch.full(
+            (1, missing, 1),
+            self.tts_model.machine.token_ids.zero,
+            dtype=torch.long,
+            device=self.tts_model.lm.device,
+        )
+        frame = self.lm_gen.step(input_tokens)
+        self.offset += 1
+        if frame is not None:
+            if self.on_frame is not None:
+                self.on_frame(frame)
+
+    def append_entry(self, entry):
+        self.state.entries.append(entry)
---
+++ code/arlab_speech_controller/arlab_speech_controller/moshi_tts_node.py
@@ -0,0 +1,204 @@
+import queue
+
+import numpy as np
+import rclpy
+import sounddevice as sd
+import torch
+from moshi.models.loaders import CheckpointInfo
+from moshi.models.tts import (
+    DEFAULT_DSM_TTS_REPO,
+    DEFAULT_DSM_TTS_VOICE_REPO,
+    Entry,
+    TTSModel,
+    script_to_entries,
+)
+from rcl_interfaces.msg import ParameterDescriptor
+from rclpy.node import Node
+from std_msgs.msg import String
+
+from .moshi_tts_gen import TTSGen
+
+
+def prepare_script(model: TTSModel, script: str, first_turn: bool) -> list[Entry]:
+    multi_speaker = first_turn and model.multi_speaker
+    return script_to_entries(
+        model.tokenizer,
+        model.machine.token_ids,
+        model.mimi.frame_rate,
+        [script],
+        multi_speaker=multi_speaker,
+        padding_between=1,
+    )
+
+
+class MoshiTTS(Node):
+    def __init__(self):
+        super().__init__(type(self).__name__)
+        self.get_logger().info(f"{type(self).__name__} node initializing...")
+
+        self.hf_repo = (
+            self.declare_parameter(
+                "hf_repo",
+                DEFAULT_DSM_TTS_REPO,
+                descriptor=ParameterDescriptor(
+                    description="HF repo in which to look for the pretrained models."
+                ),
+            )
+            .get_parameter_value()
+            .string_value
+        )
+        self.voice_repo = (
+            self.declare_parameter(
+                "voice_repo",
+                DEFAULT_DSM_TTS_VOICE_REPO,
+                descriptor=ParameterDescriptor(
+                    description="HF repo in which to look for "
+                    "pre-computed voice embeddings."
+                ),
+            )
+            .get_parameter_value()
+            .string_value
+        )
+        self.voice = (
+            self.declare_parameter(
+                "voice",
+                "expresso/ex03-ex01_happy_001_channel1_334s.wav",
+                descriptor=ParameterDescriptor(
+                    description="The voice to use, relative to the voice repo root. "
+                    f"See {DEFAULT_DSM_TTS_VOICE_REPO}"
+                ),
+            )
+            .get_parameter_value()
+            .string_value
+        )
+
+        self.device = (
+            self.declare_parameter(
+                "device",
+                "cuda",
+                descriptor=ParameterDescriptor(
+                    description="Device on which to run, defaults to 'cuda'."
+                    f"See {DEFAULT_DSM_TTS_VOICE_REPO}"
+                ),
+            )
+            .get_parameter_value()
+            .string_value
+        )
+
+        self.tts_first_turn = True
+        self.delay_steps: int = 0
+        self.pcms_audio_queue = queue.Queue()
+
+        self._setup_tts_model()
+        self._setup_audio_output()
+
+        self._start_audio_output()
+        self._start_tts_model()
+
+        self.create_subscription(
+            String,
+            topic="tts_output",
+            callback=self._tts_output_sub_callback,
+            qos_profile=10,
+        )
+
+        self.get_logger().info(f"{type(self).__name__} node initialized.")
+
+    def _setup_tts_model(self):
+        self.get_logger().info("Loading model...")
+        checkpoint_info = CheckpointInfo.from_hf_repo(self.hf_repo)
+        self.tts_model = TTSModel.from_checkpoint_info(
+            checkpoint_info, n_q=32, temp=0.6, device=self.device
+        )
+
+        if self.voice.endswith(".safetensors"):
+            voice_path = self.voice
+        else:
+            voice_path = self.tts_model.get_voice_path(self.voice)
+        # CFG coef goes here because the model was trained with CFG distillation,
+        # so it's not _actually_ doing CFG at inference time.
+        # Also, if you are generating a dialog, you should have two voices in the list.
+        condition_attributes = self.tts_model.make_condition_attributes(
+            [voice_path], cfg_coef=2.0
+        )
+
+        def _on_frame(frame):
+            if (frame != -1).all():
+                pcm = self.tts_model.mimi.decode(frame[:, 1:, :]).cpu().numpy()
+                self.pcms_audio_queue.put_nowait(np.clip(pcm[0, 0], -1, 1))
+
+        self.tts_gen = TTSGen(
+            self.tts_model, [condition_attributes], on_frame=_on_frame
+        )
+
+    def _step_timer_callback(self):
+        if self.pcms_audio_queue.qsize() > 5:
+            return
+
+        num_entries = len(self.tts_gen.state.entries)
+        if num_entries > 0 or (
+            self.tts_gen.state.end_step is not None
+            and self.tts_gen.offset < self.tts_gen.state.end_step
+        ):
+            self.tts_gen._step()
+            self.delay_steps = (
+                self.tts_model.delay_steps + max(self.tts_model.lm.delays) + 16
+            )
+        elif self.delay_steps > 0:
+            self.tts_gen._step()
+            self.delay_steps -= 1
+        else:
+            self.tts_gen.reset_state()
+
+    def _start_tts_model(self):
+        self.tts_model.mimi.streaming_forever(1)
+        self.create_timer(0.05, self._step_timer_callback)
+
+    def _setup_audio_output(self):
+        def audio_callback(outdata, _a, _b, _c):
+            # self.get_logger().info("Audio callback received.")
+            try:
+                pcm_data = self.pcms_audio_queue.get(block=False)
+                outdata[:, 0] = pcm_data
+            except queue.Empty:
+                outdata[:] = 0
+
+        self.audio_output_stream = sd.OutputStream(
+            samplerate=self.tts_model.mimi.sample_rate,
+            blocksize=1920,
+            channels=1,
+            callback=audio_callback,
+        )
+
+    def _start_audio_output(self):
+        self.audio_output_stream.start()
+
+    def _tts_output_sub_callback(self, msg: String):
+        data = msg.data
+        self.get_logger().info(f"TTS: {data}")
+        entries = prepare_script(
+            self.tts_model, data.strip(), first_turn=self.tts_first_turn
+        )
+        self.tts_first_turn = False
+        for entry in entries:
+            self.tts_gen.append_entry(entry)
+            self.tts_gen.process()
+
+    def shutdown(self):
+        self.tts_model.mimi.reset_streaming()
+        self.audio_output_stream.stop()
+        self.audio_output_stream.close()
+
+
+@torch.no_grad()
+def main(args=None):
+    rclpy.init(args=args)
+    try:
+        node = MoshiTTS()
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        pass
+
+
+if __name__ == "__main__":
+    main()

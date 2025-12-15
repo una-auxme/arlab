aafe20f019c8c1dce809c71ee98b9d4bd24a3b2b
schmid28 <leonie1.schmidt@uni-a.de>
2025-12-15 16:28:11 +0100

    test movement for test manipulation

--- code/arlab_decision_making/arlab_decision_making/behaviours/manipulation/move_to_in_cupboard.py
+++ code/arlab_decision_making/arlab_decision_making/behaviours/manipulation/move_to_in_cupboard.py
@@ -12,14 +12,14 @@ from .generic_manipulation import GenericManipulation
 
 def get_tree() -> Behaviour:
     return Sequence(
-        name="move_to_pose_grab",
+        name="move_to_in_cupboard",
         memory=True,
         children=[
             SetupManipulationMove(),
             GenericManipulation(
-                "move_to_pose_grab",
-                "/manipulation/move_to_pose_grab_goal",
-                "/manipulation/move_to_pose_grab_result",
+                "move_to_in_cupboard",
+                "/manipulation/move_to_in_cupboard_goal",
+                "/manipulation/move_to_in_cupboard_result",
             ),
             CheckManipulationMove(),
         ],
@@ -31,15 +31,15 @@ class SetupManipulationMove(Behaviour):
         super().__init__(name=type(self).__name__)
         self.blackboard = self.attach_blackboard_client(name=self.name)
         self.blackboard.register_key(
-            key="/manipulation/move_to_pose_grab_goal", access=Access.WRITE
+            key="/manipulation/move_to_in_cupboard_goal", access=Access.WRITE
         )
 
     def update(self):
         goal = ManipulationAction.Goal()
         goal.command.command_type = ManipulationCommand.COMMAND_MOVE
-        goal.command.target_pose.position.x = -0.4322
-        goal.command.target_pose.position.y = 0.0608
-        goal.command.target_pose.position.z = 0.00424
+        goal.command.target_pose.position.x = -0.0969
+        goal.command.target_pose.position.y = 0.4099
+        goal.command.target_pose.position.z = 0.12868
         # roll = -1.571
         # pitch = -2.029
         # yaw = 0.892
@@ -47,10 +47,10 @@ class SetupManipulationMove(Behaviour):
         # (quat.w, quat.x, quat.y, quat.z) = euler2quat(roll, pitch, yaw)
         # quat = goal.command.target_pose.orientation
         # (quat.w, quat.x, quat.y, quat.z) = euler2quat(0.0, pi, 0.0)
-        goal.command.target_pose.orientation._x = 0.5616
-        goal.command.target_pose.orientation._y = -0.41149
-        goal.command.target_pose.orientation._z = -0.46496
-        goal.command.target_pose.orientation._w = 0.546879
+        goal.command.target_pose.orientation._x = -0.28162
+        goal.command.target_pose.orientation._y = 0.714643
+        goal.command.target_pose.orientation._z = 0.600243
+        goal.command.target_pose.orientation._w = -0.222892
         self.blackboard.manipulation.move_to_picture_pose_grab_goal = goal
         return Status.SUCCESS
 
@@ -60,12 +60,12 @@ class CheckManipulationMove(Behaviour):
         super().__init__(name=type(self).__name__)
         self.blackboard = self.attach_blackboard_client(name=self.name)
         self.blackboard.register_key(
-            key="/manipulation/move_to_pose_grab_result", access=Access.READ
+            key="/manipulation/move_to_in_cupboard_result", access=Access.READ
         )
 
     def update(self):
         result: ManipulationAction.Result = (
-            self.blackboard.manipulation.move_to_pose_grab_result
+            self.blackboard.manipulation.move_to_in_cupboard_result
         )
         print(result.response.message)
         if result.response.message != "SUCCESS":
--- code/arlab_decision_making/arlab_decision_making/behaviours/test_trees/test_manipulation_video_sequence.py
+++ code/arlab_decision_making/arlab_decision_making/behaviours/test_trees/test_manipulation_video_sequence.py
@@ -2,7 +2,7 @@ from py_trees.behaviour import Behaviour
 from py_trees.composites import Sequence
 from py_trees.timers import Timer
 
-from ..manipulation import home, move_to_picture_pose_grab, move_to_pose_grab
+from ..manipulation import home, move_to_picture_pose_grab, move_to_pose_grab, move_to_pose_cupboard_picture, move_to_in_cupboard
 
 
 def get_tree() -> Behaviour:
@@ -15,7 +15,15 @@ def get_tree() -> Behaviour:
             Timer("MoveWait", 5.0),
             move_to_picture_pose_grab.get_tree(),
             Timer("Wait after picture", 5.0),
-            # move_to_pose_grab.get_tree(),
-            # Timer("Waiting to grab object", 5.0),
+            move_to_pose_grab.get_tree(),
+            Timer("Waiting to grab object", 5.0),
+            move_to_picture_pose_grab.get_tree(),
+            Timer("Wait after grab for picture", 5.0),
+            move_to_pose_cupboard_picture.get_tree(),
+            Timer("Wait for cupbaord picture"),
+            move_to_in_cupboard.get_tree(),
+            Timer("Waiting in cupboard", 5.0),
+            move_to_pose_cupboard_picture.get_tree(),
+            Timer("Wait for cupbaord picture"),
         ],
     )

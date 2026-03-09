473c0ff0fe968970a2feb9ed804fc07502c73595
Peter Viechter <peter.viechter@student.uni-augsburg.de>
2026-03-09 15:08:12 +0000

    Set info log level

--- code/arlab_computer_vision/launch/object_detection_launch.py
+++ code/arlab_computer_vision/launch/object_detection_launch.py
@@ -30,7 +30,7 @@ def generate_launch_description():
             ("camera_point_cloud", "/camera_gripper/depth/color/points"),
         ],
         parameters=[
-            {"log_level": "DEBUG"},  # Node-Parameter für Debug-Logs
+            {"log_level": "INFO"},  # Node-Parameter für Debug-Logs
             {"sync_tolerance": 5.0},
             {"target_frame": "world"},
             {"snapshot_mode": True},
@@ -38,7 +38,7 @@ def generate_launch_description():
         arguments=[
             "--ros-args",
             "--log-level",
-            "ObjectDetection:=debug",  # Nur unser Node auf DEBUG
+            "ObjectDetection:=info",  # Nur unser Node auf DEBUG
             "--log-level",
             "rcl:=warn",  # ROS2 interne Logs auf WARN
             "--log-level",

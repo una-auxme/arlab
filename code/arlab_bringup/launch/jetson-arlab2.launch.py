4391f1c6af01e1f037aaa137e4f28fcdaa8a912f
Peter Viechter <peter.viechter@student.uni-augsburg.de>
2026-03-11 16:27:46 +0000

    Add arlab2+3 launch files

---
+++ code/arlab_bringup/launch/jetson-arlab2.launch.py
@@ -0,0 +1,18 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    """Generate a ROS2 launch description for orchestrator nodes.
+
+    Returns:
+        LaunchDescription: LaunchDescription object containing all nodes.
+    """
+
+    voice_launch = Node(
+        package="arlab_speech_controller",
+        executable="moshi_tts",
+        parameters=[{"voice": "/workspace/src/arlab/Fast Lars opmitized 9s.wav"}],
+    )
+
+    return LaunchDescription([voice_launch])

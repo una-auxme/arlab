f0af5b08c5d22a788e3ccb675e13f5abe00607dc
Jonas Platzer <jonas@platzer.name>
2026-03-20 14:16:21 +0100

    formatting

--- code/arlab_movement/setup.py
+++ code/arlab_movement/setup.py
@@ -1,5 +1,6 @@
 import os
 from glob import glob
+
 from setuptools import find_packages, setup
 
 package_name = "arlab_movement"
@@ -11,17 +12,17 @@ setup(
     data_files=[
         ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
         ("share/" + package_name, ["package.xml"]),
-        ('share/' + package_name + '/launch', ['launch/arlab_movement.launch.py']),
-        ('share/' + package_name + '/launch', ['launch/arlab_simulation.launch.py']),
-        ('share/' + package_name + '/launch', ['launch/custom_spawn_turtlebot3.launch.py']),
-        ('share/' + package_name + '/launch', ['launch/nav2.launch.py']),
-        ('share/' + package_name + '/launch', ['launch/slam_async.launch.py']),
+        ("share/" + package_name + "/launch", ["launch/arlab_movement.launch.py"]),
+        ("share/" + package_name + "/launch", ["launch/arlab_simulation.launch.py"]),
+        ("share/" + package_name + "/launch", ["launch/custom_spawn_turtlebot3.launch.py"]),
+        ("share/" + package_name + "/launch", ["launch/nav2.launch.py"]),
+        ("share/" + package_name + "/launch", ["launch/slam_async.launch.py"]),
         # entries for arlab_simulation
-        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
-        (os.path.join('share', package_name, 'worlds'), glob('worlds/*.sdf')),
-        (os.path.join('share', package_name, 'urdf'), glob('urdf/*.urdf*')),
-        (os.path.join('share', package_name, 'rviz'), glob('rviz/*.rviz')),
-        (os.path.join('share', package_name, 'params'), glob('params/*.yaml')),
+        (os.path.join("share", package_name, "launch"), glob("launch/*.launch.py")),
+        (os.path.join("share", package_name, "worlds"), glob("worlds/*.sdf")),
+        (os.path.join("share", package_name, "urdf"), glob("urdf/*.urdf*")),
+        (os.path.join("share", package_name, "rviz"), glob("rviz/*.rviz")),
+        (os.path.join("share", package_name, "params"), glob("params/*.yaml")),
     ],
     install_requires=["setuptools"],
     zip_safe=True,
@@ -31,7 +32,8 @@ setup(
     license="MIT",
     tests_require=["pytest"],
     entry_points={
-        "console_scripts": ["dummy = arlab_movement.dummy:main",
+        "console_scripts": [
+            "dummy = arlab_movement.dummy:main",
             "arlab_movement_test = arlab_movement.arlab_movement_test:main",
             "arlab_movement_orchestrator = arlab_movement.arlab_movement_orchestrator:main",
             "navigation_stack_manager = arlab_movement.navigation_stack_manager:main",

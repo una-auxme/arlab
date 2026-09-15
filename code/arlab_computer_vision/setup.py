4a093a9b3edd2027eb7f0a1b56288e1af4ffa0d2
Simeon Wagner <73124165+Simeon-Wagner@users.noreply.github.com>
2026-09-15 13:17:33 +0200

    Labels cv kb (#242)
    * Configure object detection models via Yaml script
    
    * Dynamically loading models from yaml
    
    * Dynamic loading of labels
    
    * First comment revision
    
    * Finish commenting
    
    * Forgot formatting with ruff
    
    * Forgot formatting with ruff

--- code/arlab_computer_vision/setup.py
+++ code/arlab_computer_vision/setup.py
@@ -13,6 +13,7 @@ setup(
         ("share/" + package_name, ["package.xml"]),
         ("share/" + package_name + "/launch", glob("launch/*.py")),
         ("share/" + package_name + "/yolo_weights", glob("yolo_weights/*.pt")),
+        ("share/" + package_name + "/config", glob("config/*.yaml")),
     ],
     install_requires=["setuptools"],
     zip_safe=True,
